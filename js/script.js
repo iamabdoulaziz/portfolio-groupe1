4c3e1e80eec4f8b9d13f8e087ef2db623ab1fc86
iamabdoulaziz <abdoulazizc867@gmail.com>
2026-03-03 15:43:07 +0000

    retour à la version où tout fonctionne

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ window.addEventListener("load",()=>{
     setTimeout(()=>{document.querySelector(".hero").classList.add("active");},1000);
 
 })
+
+window.addEventListener("load",()=>{
+    setTimeout(()=>{document.querySelector(".our-team").classList.add("active");},1000);
+
+})
+
+const members = document.querySelectorAll(".member");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+members.forEach(member => {
+    observer.observe(member);
+});
