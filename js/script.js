36df1811fcaeb81ddf23ddd6e61829b9baf8a7f7
Abdoulaye Demba Diallo <abdoulayedembadiallo288@gmail.com>
2026-03-03 16:02:32 +0000

    correction

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@ window.addEventListener("load",()=>{
     setTimeout(()=>{document.querySelector(".hero").classList.add("active");},1000);
 
 })
+
+window.addEventListener("load",()=>{
+    setTimeout(()=>{document.querySelector(".our-team").classList.add("active");},1000);
+
+})
+
+
+const members = document.querySelectorAll(".member");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("active");
+        }
+    });
+}, {
+    threshold:0.6
+});
+
+members.forEach(member => {
+    observer.observe(member);
+});
