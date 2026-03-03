884723856693555d2024dad4769bdfd96623cb92
Hamidou Koita <hamidoulamine10@gmail.com>
2026-03-03 09:28:34 +0000

    second commit

--- js/cv-hamidou.js
+++ js/cv-hamidou.js
@@ -1,13 +1,13 @@
 function downloadCV() {
 
     const link = document.createElement("a");
-    link.href = "cv-hamidou.pdf"; 
+   // link.href = "cv-hamidou.pdf"; 
     link.download = "CV-Hamidou-Koita.pdf";
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 
-    // Message succès
+   
     alert("Le fichier est téléchargé avec succès");
 }
