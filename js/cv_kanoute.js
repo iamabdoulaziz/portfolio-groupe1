26910b708c3e528a3b930e9309751ec5e6988104
Aissata <kanouteaissata001@gmail.com>
2026-03-10 15:41:03 +0000

    correction telechargement

---
+++ js/cv_kanoute.js
@@ -0,0 +1,11 @@
+let lien = document.getElementById("downloadCV");
+
+lien.addEventListener("click", function(){
+
+setTimeout(function(){
+
+alert("Le fichier est téléchargé avec succès");
+
+},1000);
+
+});
