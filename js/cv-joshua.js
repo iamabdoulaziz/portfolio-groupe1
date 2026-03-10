52ea07405cf907c00bf8b4d292cdf7069b1b4348
Famory_Josue_Sissoko <famoryjosuesissoko@gmail.com>
2026-03-10 09:45:15 +0000

    joshua full version

--- js/cv-joshua.js
+++ js/cv-joshua.js
@@ -1,14 +1,14 @@
-function telechargercv()
-{const link = document.createElement('a');
-link.href = 'All CVs/CV JOSHUA.pdf';
-link.download = 'Joshua cv.pdf';
+// function telechargercv()
+// {const link = document.createElement('a');
+// link.href = 'All CVs/CV JOSHUA.pdf';
+// link.download = 'Joshua cv.pdf';
 
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
+// document.body.appendChild(link);
+// link.click();
+// document.body.removeChild(link);
 
-alert('Le fichier est téléchargé avec succès !');
-}
+// alert('Le fichier est téléchargé avec succès !');
+// }
 
 // // generate PDF from the HTML content using html2pdf library
 // function downloadPDF() {
